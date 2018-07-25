75d5fa7159b4ce64e5389d16c04d4d8207859444
JitenKumar <jitenderpalsra@gmail.com>
2018-07-26 00:04:51 +0530

    case study

---
+++ Pandas Foundation/Pandas Time Series/case_study.py
@@ -0,0 +1,75 @@
+# Print the dry_bulb_faren temperature between 8 AM and 9 AM on June 20, 2011
+print(df_clean.loc['2011-6-20 8:00:00':'2011-6-20 9:00:00','dry_bulb_faren'])
+
+# Convert the dry_bulb_faren column to numeric values: df_clean['dry_bulb_faren']
+df_clean['dry_bulb_faren'] = pd.to_numeric(df_clean['dry_bulb_faren'], errors='coerce')
+
+# Print the transformed dry_bulb_faren temperature between 8 AM and 9 AM on June 20, 2011
+print(df_clean.loc['2011-6-20 8:00:00':'2011-6-20 9:00:00','dry_bulb_faren'])
+
+# Convert the wind_speed and dew_point_faren columns to numeric values
+df_clean['wind_speed'] = pd.to_numeric(df_clean['wind_speed'], errors='coerce')
+df_clean['dew_point_faren'] = pd.to_numeric(df_clean['dew_point_faren'], errors='coerce')
+
+
+
+#########################################3
+ ########3statistics 
+ # Print the median of the dry_bulb_faren column
+print(df_clean['dry_bulb_faren'].median())
+
+# Print the median of the dry_bulb_faren column for the time range '2011-Apr':'2011-Jun'
+print(df_clean.loc['2011-Apr':'2011-Jun', 'dry_bulb_faren'].median())
+
+# Print the median of the dry_bulb_faren column for the month of January
+print(df_clean.loc['2011-Jan','dry_bulb_faren' ].median())
+
+##############################################33
+
+
+# Downsample df_clean by day and aggregate by mean: daily_mean_2011
+daily_mean_2011 = df_clean.resample('D').mean()
+
+# Extract the dry_bulb_faren column from daily_mean_2011 using .values: daily_temp_2011
+daily_temp_2011 = daily_mean_2011['dry_bulb_faren'].values
+
+# Downsample df_climate by day and aggregate by mean: daily_climate
+daily_climate = df_climate.resample('D').mean()
+
+# Extract the Temperature column from daily_climate using .reset_index(): daily_temp_climate
+daily_temp_climate = daily_climate.reset_index()['Temperature']
+
+# Compute the difference between the two arrays and print the mean difference
+difference = daily_temp_2011 - daily_temp_climate
+print(difference.mean())
+
+#--------------------------------------
+
+# Select days that are sunny: sunny
+sunny = df_clean.loc[df_clean['sky_condition']== 'CLR'] 
+
+# Select days that are overcast: overcast
+overcast = df_clean.loc[df_clean['sky_condition'].str.contains('OVC')]
+
+# Resample sunny and overcast, aggregating by maximum daily temperature
+sunny_daily_max = sunny.resample('D').max()
+overcast_daily_max = overcast.resample('D').max()
+
+# Print the difference between the mean of sunny_daily_max and overcast_daily_max
+print(sunny_daily_max.mean() - overcast_daily_max.mean())
+
+#-------------------------------------------
+
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Select the visibility and dry_bulb_faren columns and resample them: weekly_mean
+weekly_mean = df_clean[['visibility','dry_bulb_faren']].resample('W').mean()
+
+# Print the output of weekly_mean.corr()
+print(weekly_mean.corr())
+
+# Plot weekly_mean with subplots=True
+weekly_mean.plot(subplots=True)
+plt.show()
